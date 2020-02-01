ac77b6ba6ade65e17d7867276ce71afdbe835290
Adrian Samaniego <llathander@gmail.com>
2020-02-01 02:57:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
-
-
+  
 def reverse_each_word(sentence)
-  array = []
-  sentence.split.collect |word| 
-  array << word.reverse.join(" ")
-  array
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
